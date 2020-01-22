7aa2918737e99a797db5fd5b7d12bea30617c31b
Edwin Dominguez <edwindominguez@hotmail.com>
2020-01-22 16:43:45 -0500

    add header and footer components

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  section.hero.is-success
+    .hero-head
+      header.nav
+        .container
+          .nav-left
+            .nav-item
+              strong 📻 Platzi Music
+          .nav-right.nav-menu
+    .hero-body
+      .container.has-text-centered
+        h1.title Platzi Music
+        h2.subtitle Canciones
+          span.has-text-black  Vue
+          span nas
+</template>
