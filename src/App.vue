ccbe936e134fec1bff9e23b3bc0308cef5787afb
Edwin Dominguez <edwindominguez@hotmail.com>
2020-01-22 22:43:07 -0500

    add track and loader component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template lang="pug">
   #app
     pm-header
-    section.section
+    pm-loader(v-show="isLoading")
+    section.section(v-show="!isLoading")
       nav.nav.has-shadow
         .container
           .columns
@@ -16,25 +17,31 @@
             p.column
               small {{ searchMessage }}
       .container.results
-        .columns(v-for="t in tracks")
-          .column {{ t.name }} - {{ t.artists[0].name}}
+        .columns.is-multiline
+          .column.is-one-quarter(v-for="t in tracks")
+            pm-track(:track="t")
     pm-footer
 </template>
 
 <script>
-import trackService from './services/platzi-music'
-import PmFooter from './components/layout/Footer.vue'
-import PmHeader from './components/layout/Header.vue'
+import trackService from '@/services/platzi-music'
+import PmFooter from '@/components/layout/Footer.vue'
+import PmHeader from '@/components/layout/Header.vue'
+
+import PmTrack from '@/components/Track.vue'
+import PmLoader from '@/components/shared/Loader.vue'
 
 export default {
   name: 'app',
 
-  components: { PmFooter, PmHeader },
+  components: { PmFooter, PmHeader, PmTrack, PmLoader },
 
   data() {
     return {
       searchQuery: '',
-      tracks: []
+      tracks: [],
+
+      isLoading: false
     }
   },
 
@@ -48,9 +55,12 @@ export default {
     search() {
       if (!this.searchQuery) return
 
-      trackService
-        .search(this.searchQuery)
-        .then(res => (this.tracks = res.tracks.items))
+      this.isLoading = true
+
+      trackService.search(this.searchQuery).then(res => {
+        this.tracks = res.tracks.items
+        this.isLoading = false
+      })
     }
   }
 }
